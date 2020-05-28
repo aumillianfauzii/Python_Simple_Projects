d4ab7eea0039eb6f681e6358bda9b3564d115fb5
Aumillian Fauzi N <45330204+aumillianfauzii@users.noreply.github.com>
2020-05-28 22:42:06 +0700

    Create Registration.py
    u need installed moduled tkinter and openpyxl at your command prompt
    - pip install tkinter
    - pip install openpyxl

---
+++ Apps/Registration.py
@@ -0,0 +1,69 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+from openpyxl import *
+from tkinter.messagebox import showinfo
+
+win= tk.Tk()
+win.title('Registration Form ')
+
+def save():
+    f_name = entry.get()
+    l_name = entry1.get()
+    age = entry2.get()
+    email= entry3.get()
+
+    wb = Workbook()
+    ws = wb.active
+
+    ws['A1'] = " First Name "
+    ws['B1'] = " Last Name "
+    ws['C1'] = " Age "
+    ws['D1'] = " Email "
+    ws['A2'] = f_name
+    ws['B2'] = l_name
+    ws['C2'] = age
+    ws['D2'] = email
+
+    wb.save(r'E:\Python\Latihan\Registration.xlsx')
+    showinfo('Saved', "Your Entry Has Been Saved")
+
+def clear():
+    entry.delete(0, tk.END)
+    entry1.delete(0, tk.END)
+    entry2.delete(0, tk.END)
+    entry3.delete(0, tk.END)
+
+label= tk.Label(win,text='First Name: ')
+label.grid(row=0,column=0, padx=8, pady=8)
+
+entry = tk.Entry(win)
+entry.grid(row=0,column=1, padx=8, pady=8)
+
+label1 =tk.Label(win,text='Last Name: ')
+label1.grid(row=1,column=0, padx=8, pady=8)
+
+entry1 = tk.Entry(win)
+entry1.grid(row=1,column=1, padx=8, pady=8)
+
+label2 = tk.Label(win,text='Age: ')
+label2.grid(row=2,column=0, padx=8, pady=8)
+
+entry2 = tk.Entry(win)
+entry2.grid(row=2,column=1, padx=8, pady=8)
+
+label3 = tk.Label(win,text='Email: ')
+label3.grid(row=3,column=0, padx=8, pady=8)
+
+entry3 = tk.Entry(win)
+entry3.grid(row=3,column=1, padx=8, pady=8)
+
+button= ttk.Button(win,text='Register', command = save)
+button.grid(row=4, column=0, padx=8, pady=8)
+
+button2= ttk.Button(win,text='Clear', command = clear)
+button2.grid(row=4, column=1, padx=1, pady=10)
+
+win.mainloop()
+
+
+
